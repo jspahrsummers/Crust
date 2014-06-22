a637d500807ca316b50b1faeb6c2cb3bb5c9771a
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-06-21 22:44:22 -0700

    Bootstrap simple Swift framework project

---
+++ Crust/Crust.h
@@ -0,0 +1,19 @@
+//
+//  Crust.h
+//  Crust
+//
+//  Created by Justin Spahr-Summers on 2014-06-21.
+//  Copyright (c) 2014 Justin Spahr-Summers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Crust.
+FOUNDATION_EXPORT double CrustVersionNumber;
+
+//! Project version string for Crust.
+FOUNDATION_EXPORT const unsigned char CrustVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Crust/PublicHeader.h>
+
+
