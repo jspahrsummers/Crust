6823e553d52efd2bc88479830dc23d2c42edea82
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-06-22 02:06:33 -0700

    i have no idea what i'm doing dog

---
+++ Crust/MyPlayground.playground/section-1.swift
@@ -0,0 +1,17 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+struct TestModel {
+	struct name {
+		var value: String
+	}
+
+	var createdAt: struct createdAt {
+		var value: NSDate
+	}
+	
+	
+}
+
+var model = TestModel(createdAt: createdAt(NSDate()))
--- Crust/Property.swift
+++ Crust/Property.swift
@@ -8,25 +8,69 @@
 
 import Foundation
 
-struct Property<T> {
+struct ConstructedProperty<T> {
+	var value: T? = nil
+}
+
+protocol Property {
+	typealias Value
+
+	var name: String { get }
+	func construct() -> ConstructedProperty<Value>
+}
+
+struct PropertyOf<T>: Property {
+	typealias Value = T
 	let name: String
-	
-	let _getter: () -> T
-	let _setter: T -> ()
 
-	var value: T {
-		get {
-			return _getter()
-		}
-
-		set(newValue) {
-			_setter(newValue)
-		}
-	}
-
-	init(name: String, inout value: T) {
+	init(_ name: String) {
 		self.name = name
-		_getter = { value }
-		_setter = { value = $0 }
 	}
+
+	func construct() -> ConstructedProperty<T> {
+		return ConstructedProperty()
+	}
+}
+
+func construct<A>(properties: (PropertyOf<A>)) -> (ConstructedProperty<A>) {
+	return (properties.0.construct())
+}
+
+func construct<A, B>(properties: (PropertyOf<A>, PropertyOf<B>)) -> (ConstructedProperty<A>, ConstructedProperty<B>) {
+	return (properties.0.construct(), properties.1.construct())
+}
+
+func collect<A>(properties: (PropertyOf<A>)) -> Property[] {
+	return [properties.0]
+}
+
+func collect<A, B>(properties: (PropertyOf<A>, PropertyOf<B>)) -> Property[] {
+	return [properties.0, properties.1]
+}
+
+struct TestModel {
+	static let properties = (
+		PropertyOf<String>("name"),
+		PropertyOf<NSDate>("createdAt")
+	)
+
+	static func propertyNames() -> String[] {
+		return collect(properties).map { $0.name }
+	}
+
+	var properties = construct(TestModel.properties)
+
+	/*
+	mutating func setValue<EV: TypeEquality where EV.From == Property, EV.To == name>(value: String, _ ev: EV) {
+		_name.value = value
+	}
+
+	mutating func setValue<createdAt>(value: NSDate) {
+		_createdAt.value = value
+	}
+
+	func getProperties() -> TypeEquality[] {
+		return [ PropertyRefl<name>(), PropertyRefl<createdAt>() ]
+	}
+	*/
 }
