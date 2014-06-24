40bff4d3ff6a36e0aad5151c02a8849cbc34cbea
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-06-23 23:32:05 -0700

    Some better naming

--- Crust/Model.swift
+++ Crust/Model.swift
@@ -8,6 +8,46 @@
 
 import Foundation
 
+/// Describes a property's key and type.
+protocol PropertyDescription: Equatable, Hashable {
+	typealias Value
+
+	var key: String { get }
+	var type: Any.Type { get }
+}
+
+/// Describes a property of type T.
+struct PropertyOf<T>: PropertyDescription {
+	typealias Value = T
+
+	let key: String
+	let type: Any.Type = T.self
+	
+	var hashValue: Int {
+		get {
+			return key.hashValue
+		}
+	}
+
+	init(_ key: String) {
+		self.key = key
+	}
+
+	init(_ property: PropertyDescription) {
+		key = property.key
+		type = property.type
+	}
+}
+
+@infix
+func ==<T>(lhs: PropertyOf<T>, rhs: PropertyOf<T>) -> Bool {
+	return lhs.key == rhs.key
+}
+
+func _reify(properties: PropertyDescription[]) -> PropertyOf<Any>[] {
+	return properties.map { PropertyOf<Any>($0) }
+}
+
 /// The type of dictionary that should be used to store a Model's backing
 /// properties.
 typealias PropertyStorage = Dictionary<String, protocol<Equatable>>
@@ -15,41 +55,54 @@ typealias PropertyStorage = Dictionary<String, protocol<Equatable>>
 /// Represents a model object with dynamic property storage.
 ///
 /// This should typically be a value type.
-protocol Model: Equatable {
-	class func properties() -> Property[]
-	var _propertyStorage: PropertyStorage { get set }
+protocol Model {
+	class func describe() -> PropertyDescription[]
+	var properties: PropertyStorage { get set }
+
+	init(properties: PropertyStorage)
 }
 
 /// Gets the value of the given property from the given model.
-func getProperty<M: Model, T, P: Property where P.Value == T>(model: M, property: P) -> T? {
-	assert(contains(_reify(model.dynamicType.properties()), PropertyOf(property)))
+func getProperty<M: Model, T: Equatable, P: PropertyDescription where P.Value == T>(model: M, property: P) -> T? {
+	assert(contains(_reify(model.dynamicType.describe()), PropertyOf(property)))
 
-	return model._propertyStorage[property.key] as T?
+	return model.properties[property.key] as T?
+}
+
+/// Instantiates a new model that will have the given property set to the given
+/// new value.
+func transformProperty<M: Model, T: Equatable, P: PropertyDescription where P.Value == T>(model: M, property: P, value: T) -> M? {
+	assert(contains(_reify(model.dynamicType.describe()), PropertyOf(property)))
+
+	var properties = model.properties
+	properties[property.key] = value
+
+	return model.dynamicType(properties: properties)
 }
 
 /// Sets the value of the given property on the given model.
-func setProperty<M: Model, T: Equatable, P: Property where P.Value == T>(inout model: M, property: P, toValue: T) {
-	assert(contains(_reify(model.dynamicType.properties()), PropertyOf(property)))
+func setProperty<M: Model, T: Equatable, P: PropertyDescription where P.Value == T>(inout model: M, property: P, toValue: T) {
+	assert(contains(_reify(model.dynamicType.describe()), PropertyOf(property)))
 
-	model._propertyStorage[property.key] = toValue
+	model.properties[property.key] = toValue
 }
 
 /// Clears any existing value for the given property on the given model.
-func clearProperty<M: Model>(inout model: M, property: Property) {
-	assert(contains(_reify(model.dynamicType.properties()), PropertyOf(property)))
+func clearProperty<M: Model>(inout model: M, property: PropertyDescription) {
+	assert(contains(_reify(model.dynamicType.describe()), PropertyOf(property)))
 
-	model._propertyStorage.removeValueForKey(property.key)
+	model.properties.removeValueForKey(property.key)
 }
 
 struct TestModel: Model {
 	static let name = PropertyOf<String>("name")
 	static let createdAt = PropertyOf<NSDate>("createdAt")
 
-	static func properties() -> Property[] {
+	static func describe() -> PropertyDescription[] {
 		return [name, createdAt]
 	}
 
-	var _propertyStorage = PropertyStorage()
+	var properties: PropertyStorage
 
 	mutating func testThings() {
 		getProperty(self, TestModel.name)
@@ -59,5 +112,5 @@ struct TestModel: Model {
 
 @infix
 func ==(lhs: TestModel, rhs: TestModel) -> Bool {
-	return lhs._propertyStorage == rhs._propertyStorage
+	return lhs.properties == rhs.properties
 }
