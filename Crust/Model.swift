ba3655a5bce10964afdae8f7ef85715ce8b9952f
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-06-23 23:00:25 -0700

    Generalize some stuff into a Model protocol

---
+++ Crust/Model.swift
@@ -0,0 +1,42 @@
+//
+//  Model.swift
+//  Crust
+//
+//  Created by Justin Spahr-Summers on 2014-06-23.
+//  Copyright (c) 2014 Justin Spahr-Summers. All rights reserved.
+//
+
+import Foundation
+
+protocol Model {
+	class func properties() -> Property[]
+	var _propertyStorage: Dictionary<String, Any> { get set }
+}
+
+func getProperty<M: Model, T, P: Property where P.Value == T>(model: M, property: P) -> T? {
+	assert(contains(_reify(model.dynamicType.properties()), PropertyOf(property)))
+
+	return model._propertyStorage[property.key] as T?
+}
+
+func setProperty<M: Model, T, P: Property where P.Value == T>(inout model: M, property: P, toValue: T) {
+	assert(contains(_reify(model.dynamicType.properties()), PropertyOf(property)))
+
+	model._propertyStorage[property.key] = toValue
+}
+
+struct TestModel: Model {
+	static let name = PropertyOf<String>("name")
+	static let createdAt = PropertyOf<NSDate>("createdAt")
+
+	static func properties() -> Property[] {
+		return [name, createdAt]
+	}
+
+	var _propertyStorage = Dictionary<String, Any>()
+
+	mutating func testThings() {
+		getProperty(self, TestModel.name)
+		setProperty(&self, TestModel.name, "foobar")
+	}
+}
